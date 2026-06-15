92912f029cdb69e4017d5f43e60fcf336e84775c
ApoloLoxias <apololoxias@gmail.com>
2026-06-15 18:31:11 -0300

    it stopped working after I took a break and didnt touch it. This seems to work again

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/abiosoft/ishell"
 )
 
+/* main;go */
 func main() {
 	shell := ishell.New() //includees default exit, clear, help cmds
 
@@ -69,4 +71,53 @@ func fence(b1, b2 int, c *ishell.Context) {
 		hit = currenthit
 		fmt.Println(hit)
 	}
+}
+
+/* fencer.go */
+
+type Fencer struct {
+	Balance int
+	RoW     bool
+}
+
+type hitEnum string
+
+const (
+	hitONE hitEnum = "p1 hits p2"
+	hitTWO hitEnum = "p2 hits p1"
+	hitFAL hitEnum = "no hit"
+	hitDIS hitEnum = "disengage"
+)
+
+func Strike(o, d *Fencer, atk, def int) (hitEnum, error) {
+	if [2]bool{o.RoW, d.RoW} != [2]bool{true, false} {
+		return hitFAL, errors.New("invalid row pairing")
+	}
+	if atk > o.Balance || def > d.Balance {
+		return hitFAL, errors.New("invalid balance spenditure")
+	}
+
+	if atk == 0 && def == 0 {
+		return hitDIS, nil
+	}
+	if atk > def {
+		return hitONE, nil
+	}
+
+	o.Balance -= atk
+	d.Balance -= def
+
+	if o.Balance == 0 && d.Balance == 0 {
+		return hitDIS, nil
+	}
+	if o.Balance == 0 && d.Balance == 0 {
+		return hitDIS, nil
+	}
+	if o.Balance == 0 { //d.Balance != 0 is implied
+		return hitTWO, nil
+	}
+	if d.Balance == 0 { //o.Balance !=0 is implied
+		return hitONE, nil
+	}
+	return hitFAL, nil
 }
