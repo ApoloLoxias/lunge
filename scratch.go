e4721357ad0a1a2d03c579647601c74573abf06d
ApoloLoxias <apololoxias@gmail.com>
2026-06-24 23:21:20 -0300

    self-drawing screen prototype done

--- scratch.go
+++ scratch.go
@@ -5,8 +5,10 @@ package main
 
 func main() {
 	screen := screen{
-		lines:   []string{"line 1", "line 2", "", "line 4"},
-		borders: [3]string{"------", "|", "------"},
+		lines:   []string{"line 1", "line     2", "", "line 4   "},
+		borders: [2]string{"-", "|"},
+		height:  10,
+		length:  20,
 	}
 	screen.draw()
 
--- screen.go
+++ screen.go
@@ -1,19 +1,26 @@
 package main
 
 import "fmt"
+import "strings"
+import "unicode/utf8"
 
 type screen struct {
 	lines   []string
-	borders [3]string
+	borders [2]string
+	length  int
+	height  int
 }
 
 func (s screen) draw() {
 	fmt.Println()
-	fmt.Println(s.borders[0])
+	fmt.Println(strings.Repeat(s.borders[0], s.length+2))
 	for _, line := range s.lines {
-		fmt.Println(s.borders[1] + line + s.borders[1])
+		filler := strings.Repeat(" ", s.length-utf8.RuneCountInString(line))
+		fmt.Println(s.borders[1] + line + filler + s.borders[1])
 	}
-	fmt.Println(s.borders[2])
+	filler := s.borders[1] + strings.Repeat(" ", s.length) + s.borders[1] + "\n"
+	fmt.Print(strings.Repeat(filler, s.height))
+	fmt.Println(strings.Repeat(s.borders[0], s.length+2))
 }
 
 //type line string
